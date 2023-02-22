8e8eedff95a018252ecf888e63c0eb29487d0678
LynnT-2003 <lnyinyi22@gmail.com>
2023-02-22 13:57:45 +0700

    Created blogs page

---
+++ pages/blogs.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+export default function Home() {
+
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    // fetch data from API
+    fetch("/api/blogs/articles")
+      .then(res => res.json())
+      .then(data => {
+        // do something with data
+        console.log(data)
+        setBlogs(data)
+      })
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Blogs</title>
+      </Head>
+      <h1>Blogs</h1>
+      <p>
+        <ul>
+          {
+            blogs.map(blog => {
+              return (
+                <li key={blog._id}> {blog.title}</li>
+              )
+            })
+          }
+        </ul>
+      </p>
+
+    </>
+  )
+}
