08a8e2e5476f8e9b071950e34ba9cb22b9658dae
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-02-26 13:17:11 +0700

    Added stocks/products page (stocks id not working)

---
+++ pages/stocks/index.jsx
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+export default function Home({ stocks }) {
+
+  function deleteStock(id) {
+    fetch(`https://stock-next-new.vercel.app/api/stocks/products/${id}`,
+      {
+        method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+        // alert("Deleting " + id)
+        window.location.reload(false);
+      })
+
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Stocks</title>
+      </Head>
+      <h1>Stocks</h1>
+      <table><tbody>
+        {
+          stocks.map(stock => {
+            return (
+              <tr key={stock._id}>
+                <td>
+                  <Link href={`/stocks/${stock._id}`}>
+                    {stock.code} 
+                  </Link>
+                </td>
+                <td>
+                  <button onClick={() => deleteStock(stock._id)}>Delete</button>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+      </table>
+      <p>
+      </p>
+
+    </>
+  )
+}
+export async function getServerSideProps() {
+  const res = await fetch(`https://stock-next-new.vercel.app/api/stocks/products/`)
+  const stocks = await res.json()
+  return { props: { stocks } }
+}
