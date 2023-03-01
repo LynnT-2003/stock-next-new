09ecd210f6f1d4dbd7a689c17931d84a2ac55065
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-01 14:58:03 +0700

    Formatting

--- pages/blogs/[id].jsx
+++ pages/blogs/[id].jsx
@@ -1,15 +1,16 @@
-import Head from "next/head"
-import Link from "next/link"
+import Head from "next/head";
+import Link from "next/link";
 
 // Step 2: This component is rendered from the server (Server-Side Rendering) SSR
 export default function Blog({ blog }) {
-  console.log('blog 2', blog)
-  if (!blog) return (
-    <div>
-      <p>Blog not found</p>
-      <Link href="/blogs">Back</Link>
+  console.log("blog 2", blog);
+  if (!blog)
+    return (
+      <div>
+        <p>Blog not found</p>
+        <Link href="/blogs">Back</Link>
       </div>
-  );
+    );
 
   return (
     <>
@@ -20,14 +21,16 @@ export default function Blog({ blog }) {
       <p>{blog.content}</p>
       <Link href="/blogs">Back</Link>
     </>
-  )
+  );
 }
 
 // STEP 1: This function will be executed at the server before loading the page.
 export async function getServerSideProps({ params }) {
-  console.debug('params', params)
-  const res = await fetch(`https://stock-next-new.vercel.app/api/blogs/articles/${params.id}`)
-  const blog = await res.json()
-  console.debug('blog 1', blog)
-  return { props: { blog } }
+  console.debug("params", params);
+  const res = await fetch(
+    `https://stock-next-new.vercel.app/api/blogs/articles/${params.id}`
+  );
+  const blog = await res.json();
+  console.debug("blog 1", blog);
+  return { props: { blog } };
 }
