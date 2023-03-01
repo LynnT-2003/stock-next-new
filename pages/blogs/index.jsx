62a506fd7ea2911c9753e11d469172843ff7b395
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-01 14:57:45 +0700

    Formatting

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -1,20 +1,17 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home({ blogs }) {
-
   function deleteBlog(id) {
-    fetch(`https://stock-next-new.vercel.app/api/blogs/articles/${id}`,
-      {
-        method: 'DELETE'
-      })
-      .then(res => res.json())
-      .then(data => {
+    fetch(`https://stock-next-new.vercel.app/api/blogs/articles/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
         // alert("Deleting " + id)
         window.location.reload(false);
-      })
-
+      });
   }
 
   return (
@@ -23,34 +20,31 @@ export default function Home({ blogs }) {
         <title>Blogs</title>
       </Head>
       <h1>Blogs</h1>
-      <table><tbody>
-        {
-          blogs.map(blog => {
+      <table>
+        <tbody>
+          {blogs.map((blog) => {
             return (
               <tr key={blog._id}>
                 <td>
-                  <Link href={`/blogs/${blog._id}`}>
-                    {blog.title}
-                  </Link>
+                  <Link href={`/blogs/${blog._id}`}>{blog.title}</Link>
                 </td>
                 <td>
                   <button onClick={() => deleteBlog(blog._id)}>Delete</button>
                 </td>
               </tr>
-            )
-          })
-        }
-      </tbody>
+            );
+          })}
+        </tbody>
       </table>
-      <p>
-      </p>
-
+      <p></p>
     </>
-  )
+  );
 }
 export async function getServerSideProps() {
-  const res = await fetch(`https://stock-next-new.vercel.app/api/blogs/articles/`)
-  const blogs = await res.json()
+  const res = await fetch(
+    `https://stock-next-new.vercel.app/api/blogs/articles/`
+  );
+  const blogs = await res.json();
   // console.debug('blog 1', blogs)
-  return { props: { blogs } }
+  return { props: { blogs } };
 }
